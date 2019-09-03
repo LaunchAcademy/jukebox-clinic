d5d5f4df976fb48d4fcff1c4964f495ad7f2d5d3
Patrick Whelan <patrickawhelan@gmail.com>
2019-09-03 14:46:50 -0400

    added setup

---
+++ code.rb
@@ -0,0 +1,21 @@
+available_songs = [
+  "Hello - Lionel Ritchie",
+  "Kokomo – The Beach Boys",
+  "Girl You Know It’s True – Milli Vanilli",
+  "Agadoo – Black Lace",
+  "Down Under - Men at Work",
+  "Nothing's Gonna Stop Us Now - Starship",
+  "Get Outta My Dreams, Get Into My Car - Billy Ocean",
+  "I Just Called To Say I Love You - Stevie Wonder",
+  "Hangin' Tough - New Kids on the Block",
+  "We Built This City - Starship",
+  "Wake Me Up Before You Go Go - Wham!",
+  "We Didn't Start The Fire - Billy Joel",
+  "I Wanna Dance With Somebody - Whitney Houston",
+  "U Can't Touch This - MC Hammer"
+]
+
+requested_songs = [
+  "Wake Me Up Before You Go Go - Wham!",
+  "She Don't Love Me Anymore - The Aliens"
+]
---
+++ lib/jukebox.rb
@@ -0,0 +1,13 @@
+class Jukebox
+  def initialize(available_songs, requested_songs)
+    # Your code here
+  end
+
+  def add_track!(new_track)
+    # Your code here
+  end
+
+  def play!
+    # Your code here
+  end
+end
