1e8ec15494d16fa07631ecf809d4ec9a338c1831
dovakeen118 <kegillis118@gmail.com>
2019-12-12 16:38:41 -0500

    Jukebox method names changed to match later versions

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,11 +3,11 @@ class Jukebox
     # Your code here
   end
 
-  def add_track!(new_track)
+  def add_track_to_playlist!(new_track)
     # Your code here
   end
 
-  def play!
+  def start_playlist!
     # Your code here
   end
 end
