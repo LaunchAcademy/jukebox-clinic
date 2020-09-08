6ccdf5408989f926edec76f3eb0156b24545d237
dovakeen118 <kegillis118@gmail.com>
2020-09-08 17:21:52 -0400

    finished code

--- code.rb
+++ code.rb
@@ -1,21 +1,60 @@
-library = [
-  "Hello - Lionel Ritchie",
-  "Kokomo – The Beach Boys",
-  "Girl You Know It’s True – Milli Vanilli",
-  "Agadoo – Black Lace",
-  "Down Under - Men at Work",
-  "Nothing's Gonna Stop Us Now - Starship",
-  "Get Outta My Dreams, Get Into My Car - Billy Ocean",
-  "I Just Called To Say I Love You - Stevie Wonder",
-  "Hangin' Tough - New Kids on the Block",
-  "We Built This City - Starship",
-  "Wake Me Up Before You Go Go - Wham!",
-  "We Didn't Start The Fire - Billy Joel",
-  "I Wanna Dance With Somebody - Whitney Houston",
-  "U Can't Touch This - MC Hammer"
-]
+require 'pry'
+require_relative './lib/jukebox'
+require_relative './lib/song'
 
-song_requests = [
-  "Wake Me Up Before You Go Go - Wham!",
-  "She Don't Love Me Anymore - The Aliens"
-]
+# library = [
+#   "Hello - Lionel Ritchie",
+#   "Kokomo – The Beach Boys",
+#   "Girl You Know It’s True – Milli Vanilli",
+#   "Agadoo – Black Lace",
+#   "Down Under - Men at Work",
+#   "Nothing's Gonna Stop Us Now - Starship",
+#   "Get Outta My Dreams, Get Into My Car - Billy Ocean",
+#   "I Just Called To Say I Love You - Stevie Wonder",
+#   "Hangin' Tough - New Kids on the Block",
+#   "We Built This City - Starship",
+#   "Wake Me Up Before You Go Go - Wham!",
+#   "We Didn't Start The Fire - Billy Joel",
+#   "I Wanna Dance With Somebody - Whitney Houston",
+#   "U Can't Touch This - MC Hammer"
+# ]
+
+# song_requests = [
+#   "Wake Me Up Before You Go Go - Wham!",
+#   "She Don't Love Me Anymore - The Aliens"
+# ]
+
+song1 = Song.new("Wake Me Up Before You Go Go", "Wham!")
+song2 = Song.new("She Don't Love Me Anymore", "The Aliens")
+song3 = Song.new("Hello", "Lionel Ritchie")
+song4 = Song.new("Kokomo", "The Beach Boys")
+song5 = Song.new("Nothing's Gonna Stop Us Now", "Starship")
+
+library = [song1, song2, song3, song4]
+
+song_requests = [song1, song2]
+
+jukebox = Jukebox.new(library, song_requests)
+# binding.pry
+
+puts "\nOriginal Playlist:"
+# puts jukebox.song_requests
+jukebox.song_requests.each do |song|
+  puts song.full_name
+end
+# binding.pry
+# jukebox.add_track_to_playlist!("We Built This City - Starship")
+jukebox.add_track_to_playlist!(song4)
+jukebox.add_track_to_playlist!(song5)
+
+puts "\nNew playlist:"
+# puts jukebox.song_requests
+jukebox.song_requests.each do |song|
+  puts song.full_name
+end
+
+jukebox.start_playlist!
+jukebox.start_playlist!
+jukebox.start_playlist!
+
+# binding.pry
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,13 +1,46 @@
 class Jukebox
+  attr_reader :library, :song_requests
+
   def initialize(library, song_requests)
-    # Your code here
+    @library = library
+    @song_requests = []
+    
+    # binding.pry
+    song_requests.each do |song|
+      # binding.pry
+      add_track_to_playlist!(song)
+    end
+    # binding.pry
   end
 
   def add_track_to_playlist!(new_track)
-    # Your code here
+    # binding.pry
+    # check if the song is in our library
+    if library.include?(new_track)
+      # binding.pry
+      song_requests.push(new_track)
+      # song_requests << new_track
+      # if it's available, add to song_requests and return true
+      return true
+    else
+      # otherwise, return false
+      return false
+    end
   end
 
   def start_playlist!
-    # Your code here
+    # binding.pry
+    # grab the first song
+    # current_song = @song_requests.shift
+    # binding.pry
+    # display that song as Now Playing
+    # puts "Now Playing: #{current_song}"
+    # add song to the end of the playlist
+    # @song_requests.push(current_song)
+    # binding.pry
+    # @song_requests << current_song
+    @song_requests.each do |song|
+      song.play_track!
+    end
   end
 end
